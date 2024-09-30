d9ec170f6ed794a303ecd8a16eacb09021bb940c
Lucas Whitaker <lucaswhitaker22@gmail.com>
2024-09-30 20:19:45 +0000

    added more metrics

---
+++ Assignment1/Part2/simulations/cpu_speed/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Change to the Part2 directory
+cd ./Assignment1/Part2
+
+./interrupts "simulations/cpu_speed/trace_full_speed.txt" "simulations/cpu_speed/vector_table.txt" "simulations/cpu_speed/output/execution_full.txt" 
+./interrupts "simulations/cpu_speed/trace_half_speed.txt" "simulations/cpu_speed/vector_table.txt" "simulations/cpu_speed/output/execution_half.txt" 
+./interrupts "simulations/cpu_speed/trace_quarter_speed.txt" "simulations/cpu_speed/vector_table.txt" "simulations/cpu_speed/output/execution_quarter.txt"
---
+++ Assignment1/Part2/simulations/isr/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change to the Part2 directory
+cd ./Assignment1/Part2
+
+./interrupts "simulations/isr/trace_1.txt" "simulations/isr/vector_table.txt" "simulations/isr/output/execution_1.txt" 
+./interrupts "simulations/isr/trace_2.txt" "simulations/isr/vector_table.txt" "simulations/isr/output/execution_2.txt" 
+./interrupts "simulations/isr/trace_3.txt" "simulations/isr/vector_table.txt" "simulations/isr/output/execution_3.txt" 
+./interrupts "simulations/isr/trace_4.txt" "simulations/isr/vector_table.txt" "simulations/isr/output/execution_4.txt" 
+./interrupts "simulations/isr/trace_5.txt" "simulations/isr/vector_table.txt" "simulations/isr/output/execution_5.txt" 
